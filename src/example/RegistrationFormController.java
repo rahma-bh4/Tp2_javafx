8ac850ad45fff02769fbd93105d8697f0e424fe7
rahma-bh4 <benhassine.rahma4@gmail.com>
2025-05-10 02:41:19 +0100

    adding some changes

--- src/example/RegistrationFormController.java
+++ src/example/RegistrationFormController.java
@@ -1,5 +1,63 @@
 package example;
 
-public class RegistrationFormController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Window;
 
+public class RegistrationFormController {
+    @FXML
+    private TextField nameField;
+    
+    @FXML
+    private TextField emailField;
+    
+    @FXML
+    private PasswordField passwordField;
+    
+    @FXML
+    private Button submitButton;
+    
+    @FXML
+    protected void handleSubmitButtonAction(ActionEvent event) {
+        Window owner = submitButton.getScene().getWindow();
+        
+        // Check if the name field is empty
+        if(nameField.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!", 
+                    "Please enter your name");
+            return;
+        }
+        
+        // Check if the email field is empty
+        if(emailField.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!", 
+                    "Please enter your email");
+            return;
+        }
+        
+        // Check if the password field is empty
+        if(passwordField.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!", 
+                    "Please enter a password");
+            return;
+        }
+        
+        // If all fields are filled, show success message
+        showAlert(Alert.AlertType.CONFIRMATION, owner, "Registration Successful!", 
+                "Welcome " + nameField.getText());
+    }
+    
+    private void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
 }
