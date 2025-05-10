e8cff060e05f719d74999a6e2a13066162e83e52
rahma-bh4 <benhassine.rahma4@gmail.com>
2025-05-10 02:29:12 +0100

    First commit

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module tp4 {
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.base;
+	//requires javafx.media;
+	//requires javafx.swing;
+	//requires javafx.web;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
